da1214e3bd3497a25f41dd35598916a29985fa8f
ivo <ivo.deus@looplex.com.br>
2021-09-09 08:56:57 -0300

    1.1.5 - File preparations for manual installation

--- lib/Config.py
+++ lib/Config.py
@@ -8,7 +8,7 @@ class Config:
 
     pluginName = 'Looplex_Lawtex_ST3_Plugin'
 
-    pluginJar = 'looplex_lawtex_plugin-1.3-jar-with-dependencies.jar'
+    pluginJar = 'looplex_lawtex_plugin-1.4.jar'
 
     mainDataFolder = 'Looplex_Lawtex_ST3_Plugin'
     logsDataSubFolder = 'logs'

aec6f3166c95a0a745cc0c1cb99ae26323a46ee5
ivo <ivo.deus@looplex.com.br>
2021-11-02 18:50:32 -0300

    Added save Looplex login functionality

--- lib/Change_login_context.py
+++ lib/Change_login_context.py
@@ -7,7 +7,7 @@ class Change_login_context() :
 
     def changeContext(self) :
 
-    	change_context = "{\"command\":\"CHANGE_LOGIN_CONTEXT\",\"lang\":\"pt-BR\"}"
+        change_context = "{\"command\":\"CHANGE_LOGIN_CONTEXT\",\"lang\":\"pt-BR\"}"
 
         utilCall = Config()
         utilCall.run_jar_dependency_in_background(change_context)

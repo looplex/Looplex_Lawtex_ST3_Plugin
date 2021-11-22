4932fa10a968968eb8b0fe00b0236a14ca265bf1
ivo <ivo.deus@looplex.com.br>
2021-11-22 18:06:57 -0300

    Testing embed JRE

--- lib/Config.py
+++ lib/Config.py
@@ -22,19 +22,25 @@ class Config:
 
                 with ZipFile( os.path.dirname( os.path.dirname(__file__) ), 'r') as zip_obj:
 
-                    zip_obj.extractall('java/windows', os.path.join(sublime.packages_path(), Config.pluginName))
+                    if file.startswith('java/windows/'):
+
+                        zip_obj.extract('java', os.path.join(sublime.packages_path(), Config.pluginName))
 
             elif sublime.platform() == "osx":
 
                 with ZipFile( os.path.dirname( os.path.dirname(__file__) ), 'r') as zip_obj:
 
-                    zip_obj.extractall('java/osx', os.path.join(sublime.packages_path(), Config.pluginName))
+                    if file.startswith('java/osx/'):
+
+                        zip_obj.extractall('java', os.path.join(sublime.packages_path(), Config.pluginName))
 
             else:
 
                 with ZipFile( os.path.dirname( os.path.dirname(__file__) ), 'r') as zip_obj:
 
-                    zip_obj.extractall('java/linux', os.path.join(sublime.packages_path(), Config.pluginName))
+                    if file.startswith('java/linux/'):
+
+                        zip_obj.extractall('java', os.path.join(sublime.packages_path(), Config.pluginName))
 
     def retrieve_java_dependency_filepath() :
 

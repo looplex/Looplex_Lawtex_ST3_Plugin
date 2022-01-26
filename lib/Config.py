51c256012dde13800888b2c5b3bce548b1116e2d
unknown <ivo.deus@looplex.com.br>
2022-01-26 11:32:43 -0300

    Setting up runtime on Windows

--- lib/Config.py
+++ lib/Config.py
@@ -8,7 +8,6 @@ class Config:
     pluginName = 'Looplex_Lawtex_ST3_Plugin'
 
     pluginJar = 'looplex_lawtex_plugin-1.5.jar'
-    pluginExe = 'Looplex Lawtex Plugin.exe'
 
     mainDataFolder = 'Looplex_Lawtex_ST3_Plugin'
     logsDataSubFolder = 'logs'
@@ -38,24 +37,23 @@ class Config:
 
     def run_jar_dependency_in_background(self, context) :
 
-        jar_filepath = Config.retrieve_jar_dependency_filepath()
-
         if sublime.platform() == "windows":
             CREATE_NO_WINDOW = 0x08000000
             context = context.replace("\\", "\\\\")
-            exe_filepath = Config.retrieve_windows_exe_dependency_filepath()
-            subprocess.Popen([ exe_filepath, context ], creationflags = CREATE_NO_WINDOW )
+            jar_filepath = Config.retrieve_jar_dependency_filepath( "windows" )
+            jre_filepath = Config.retrieve_jre_dependency_filepath( "windows" )
+            subprocess.Popen([ jre_filepath, '-jar', jar_filepath, context ], creationflags = CREATE_NO_WINDOW )
 
         else :
             subprocess.Popen([ "java", "-jar", jar_filepath, context ])
 
-    def retrieve_jar_dependency_filepath() :
+    def retrieve_jar_dependency_filepath(self, os_name) :
 
-        return os.path.join(sublime.packages_path(), Config.pluginName, 'jar', Config.pluginJar)
+        return os.path.join(sublime.packages_path(), Config.pluginName, 'jar', os_name, Config.pluginJar)
 
-    def retrieve_windows_exe_dependency_filepath() :
+    def retrieve_jre_dependency_filepath(self, os_name) :
 
-        return os.path.join(sublime.packages_path(), Config.pluginName, 'plugin', Config.pluginExe)
+        return os.path.join(sublime.packages_path(), Config.pluginName, 'jre', os_name, 'bin', 'java.exe')
 
     def retrieve_logs_folder_linux() :
 
